2d95d3929405d516bea781c917faefd795836d6c
Ethan Vance <128346397+Fanzekai@users.noreply.github.com>
2024-03-15 20:50:15 +0800

    Delete src/main/java/org/example/demo01/Abstractclasses directory

--- src/main/java/org/example/demo01/Abstractclasses/Animal.java
+++
@@ -1,6 +0,0 @@
-package org.example.demo01.Abstractclasses;/**
- * @author  fzk
- * @date  2024/3/15  20:20
- * @version 1.0
-*/public class Animal {
-}
--- src/main/java/org/example/demo01/Abstractclasses/Dog.java
+++
@@ -1,6 +0,0 @@
-package org.example.demo01.Abstractclasses;/**
- * @author  fzk
- * @date  2024/3/15  20:23
- * @version 1.0
-*/public class Dog {
-}
