bf5c079dce9fc50a6f2f028046e6c9aa54f72a43
fzk <1610661203@qq.com>
2024-03-16 00:35:37 +0800

    commit demo06

---
+++ src/main/java/demo6/Recursion.java
@@ -0,0 +1,23 @@
+package demo6;
+
+/**
+ * @author fzk
+ * @version 1.0
+ * @date 2023/8/28  16:11
+ */
+public class Recursion {
+    //阶乘算法
+    public int factorial(int num){
+        if (num==0 || num== 1){
+            return 1;
+        }else{
+            return num*factorial(num-1);
+        }
+    }
+    public static void main(String[] args) {
+        Recursion recursion = new Recursion();
+        int n = 4;
+        int factorial = recursion.factorial(n);
+        System.out.println(factorial);
+    }
+}
