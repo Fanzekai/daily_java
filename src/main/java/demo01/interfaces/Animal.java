7b078121df69e86f77cf06a88377fab82eeb3e01
fzk <1610661203@qq.com>
2024-03-15 21:00:58 +0800

    commit interfaces

---
+++ src/main/java/demo01/interfaces/Animal.java
@@ -0,0 +1,11 @@
+package demo01.interfaces;
+
+/**
+ * @author fzk
+ * @version 1.0
+ * @date 2024/3/15  20:53
+ */
+public interface Animal {
+    public void eat();
+    public void travel();
+}
---
+++ src/main/java/demo01/interfaces/MammalInt.java
@@ -0,0 +1,36 @@
+package demo01.interfaces;
+
+/**
+ * @author fzk
+ * @version 1.0
+ * @date 2024/3/15  20:57
+ */
+public class MammalInt implements Animal{
+
+
+    @Override
+    public void eat() {
+        System.out.println("Mammal eats");
+    }
+
+    @Override
+    public void travel() {
+        System.out.println("Mammal travels");
+    }
+
+    public int noofLegs(){
+        return 0;
+    }
+}
+
+
+/**
+ * 一个类可以同时实现多个接口。
+ * 一个类只能继承(extends)一个类，但是能实现(implements)多个接口。
+ * 一个接口只能继承一个接口，不能实现接口
+ * 一个接口能继承另一个接口，这和类之间的继承比较相似
+ * 接口是隐式抽象的，所以声明时没有必要使用abstract关键字；
+ * 接口的每个方法都是隐式抽象的，所以同样不需要使用abstract关键字；
+ * 接口中的方法都是隐式public的
+ * 接口中可以含有变量，但是接口中的变量会被隐式的指定为public static final变量（并且只能是 public，用 private 修饰会报编译错误）。
+ * **/
