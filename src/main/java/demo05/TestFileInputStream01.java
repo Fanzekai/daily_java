ed1030f2bdd5c57112a2574e087208cc3dafeb82
fzk <1610661203@qq.com>
2024-03-17 12:42:17 +0800

    commit demo05

--- src/main/java/demo05/TestFileInputStream01.java
+++ src/main/java/demo05/TestFileInputStream01.java
@@ -13,14 +13,19 @@ import java.io.IOException;
 public class TestFileInputStream01 {
     public static void main(String args[]) {
 
+
         FileInputStream in = null;
 
         try {
             in = new FileInputStream("D:\\workspace\\IDEA\\Git\\大杂烩\\java\\src\\main\\java\\demo05\\txt\\read");
-            int read = in.read();
-            System.out.println((char) read);
-            int read2 = in.read();
-            System.out.println(read2);
+
+            byte[] bytes = new byte[1024];
+
+            int read = in.read(bytes); // 将文件内容读取到字节数组中,记录一共读了多少字节
+            if (in.read(bytes) == -1) { // 此时判断的是是否已经到达文件末尾
+                System.out.println(new String(bytes, 0, read)); // 输出实际读取的内容
+            }
+
             System.out.println("剩下几个字节没有读:"+in.available());
         } catch (FileNotFoundException e) {
             System.out.println("系统找不到指定文件！");
