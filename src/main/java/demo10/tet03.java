f4ffb84828588d18a26dfe3a5cc21f8aac969b15
fzk <1610661203@qq.com>
2024-03-21 22:56:32 +0800

    commit demo10

--- src/main/java/demo10/tet03.java
+++ src/main/java/demo10/tet03.java
@@ -9,11 +9,7 @@ public class tet03 {
 
 
     public static void main(String[] args) {
-        String s1 = new String("螃蟹哥");
-        String s2= new String("螃蟹哥");
-        System.out.println(s1 == s2);
 
-        System.out.println(s1.equals(s2));
     }
 
 }
