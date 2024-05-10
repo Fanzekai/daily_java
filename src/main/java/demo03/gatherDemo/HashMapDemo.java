8a91d48d2d5d3d1f5e723f324334dcd9e5dd49a9
fzk <1610661203@qq.com>
2024-05-10 15:01:16 +0800

    commit demo03

--- src/main/java/demo03/gatherDemo/HashMapDemo.java
+++ src/main/java/demo03/gatherDemo/HashMapDemo.java
@@ -1,6 +1,42 @@
-package demo03.gatherDemo;/**
- * @author  fzk
- * @date  2024/5/5  11:16
+package demo03.gatherDemo;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * @author fzk
  * @version 1.0
-*/public class HashMapDemo {
-}
+ * @date 2024/5/5  11:16
+ */
+public class HashMapDemo {
+
+    public static void main(String[] args) {
+        // 创建一个HashMap存储学生的姓名和对应的分数
+        Map<String, Integer> scores = new HashMap<>();
+        scores.put("Alice", 75);
+        scores.put("Bob", 45);
+        scores.put("Charlie", 90);
+        scores.put("David", 55);
+
+        // 使用迭代器遍历HashMap并删除分数低于60的学生
+        Iterator<Map.Entry<String, Integer>> iterator = scores.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, Integer> entry = iterator.next();
+            if (entry.getValue() < 60) {
+                iterator.remove(); // 使用迭代器的remove方法删除元素
+            }
+        }
+
+        //报错ConcurrentModificationException异常
+        //for (String string : scores.keySet()) {
+        //    if (scores.get(string)<60) {
+        //        scores.remove(string);
+        //    }
+        //}
+
+        // 打印剩余学生的姓名和分数
+        for (Map.Entry<String, Integer> entry : scores.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }}
--- src/main/java/demo03/gatherDemo/MapDemo.java
+++ src/main/java/demo03/gatherDemo/MapDemo.java
@@ -1,6 +1,47 @@
-package demo03.gatherDemo;/**
- * @author  fzk
- * @date  2024/5/3  18:39
+package demo03.gatherDemo;
+
+import java.util.HashMap;
+
+/**
+ * @author fzk
  * @version 1.0
-*/public class MapDemo {
+ * @date 2024/5/3  18:39
+ */
+public class MapDemo {
+
+    public static void main(String[] args) {
+        // 创建 HashMap 对象
+        HashMap<String, Integer> map = new HashMap<>();
+
+        // 将键值对存储到 HashMap 中
+        map.put("apple", 10);
+        map.put("banana", 20);
+        map.put("orange", 30);
+
+        // 根据键获取对应的值
+        Integer value = map.get("apple");
+        System.out.println("Value for 'apple': " + value);
+
+        // 判断 HashMap 是否包含指定的键
+        boolean containsKey = map.containsKey("banana");
+        System.out.println("Contains key 'banana': " + containsKey);
+
+        // 判断 HashMap 是否包含指定的值
+        boolean containsValue = map.containsValue(20);
+        System.out.println("Contains value 20: " + containsValue);
+
+        // 移除 HashMap 中指定键对应的键值对
+        map.remove("apple");
+
+        // 返回 HashMap 中键值对的数量
+        int size = map.size();
+        System.out.println("Size of map: " + size);
+
+        // 判断 HashMap 是否为空
+        boolean isEmpty = map.isEmpty();
+        System.out.println("Is map empty: " + isEmpty);
+
+        // 清空 HashMap 中的所有键值对
+        map.clear();
+    }
 }
