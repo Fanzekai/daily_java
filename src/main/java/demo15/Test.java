c2b1ffc9b984bdeedb1e0e2da378df0d123c1272
fzk <1610661203@qq.com>
2024-05-17 21:08:30 +0800

    commit demo015

--- src/main/java/demo15/Test.java
+++ src/main/java/demo15/Test.java
@@ -5,15 +5,48 @@ package demo15;
  * @version 1.0
  * @date 2024/4/25  15:57
  */
-public class demo02 {
+public class Test {
+
+
+
 
     public String text;
-    public static void main(String[] arguments) {
+
+    public void test01(){
         String test1 = "This is a test";
         String test2 = "This is a test";
         System.out.println(test1 == test2);
-        Test t1 = new TestO;
+        Test t1 = new Test();
         t1.text = "This is a test";
-        Test t2 = new TestO;
-        t2.text = "This is a test",System.out.println(t1==t2);
+        Test t2 = new Test();
+        t2.text = "This is a test";
+        System.out.println(t1 == t2);
+
+        //test1和test2都是String类型的对象，它们的值都是"This is a test"，
+        // 由于Java中的字符串常量池特性，相同的字符串常量在内存中只会有一个实例，
+        // 因此test1和test2都指向字符串常量池中的同一块地址，所以test1 == test2会返回true
+    }
+
+    public static void test02() {
+
+    //    for (int i = 0; i < 3; i++) {
+    //        System.out.print(i);
+    //    }
+    //    System.out.print(i);
+    //}
+    }
+
+
+    public static void test03() {
+
+        String s="fzk";
+
+        char c=s.charAt(0);
+        System.out.println(c);
+    }
+
+    public static void main(String[] arguments) {
+
+        test03();
+    }
 }
--- src/main/java/demo15/test01.java
+++ src/main/java/demo15/test01.java
@@ -8,4 +8,27 @@ package demo15;
 public class test01 {
 
 
+
+    static char aChar1=06477;
+
+    static char c2='\uface';
+
+    static char c3=0xbeef;
+
+    //float f=1.3;
+    //char c="a";
+    //byte b=25;
+    //boolean g=null;
+
+    //static char c4=\u0041;
+
+    public static void main(String[] args) {
+        System.out.println(aChar1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+
+        System.out.println(1+34+"2"+2);
+        System.out.println("1+1"+1+34);
+    }
 }
