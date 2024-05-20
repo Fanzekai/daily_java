48b8d336119f42c0c89bef07557745c949fbce27
fzk <1610661203@qq.com>
2024-05-20 23:13:05 +0800

    commit demo02

--- src/main/java/demo02/ScheduledTasks/TimerTaskDemo.java
+++ src/main/java/demo02/ScheduledTasks/TimerTaskDemo.java
@@ -1,6 +1,32 @@
-package demo02.ScheduledTasks;/**
- * @author  fzk
- * @date  2024/5/8  11:16
+package demo02.ScheduledTasks;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * @author fzk
  * @version 1.0
-*/public class test {
+ * @date 2024/5/8  11:16
+ */
+public class TimerTaskDemo {
+
+    public static void main(String[] args) {
+
+        // 示例代码：
+        TimerTask task = new TimerTask() {
+            public void run() {
+                System.out.println("当前时间: " + new Date() + "n" +
+                        "线程名称: " + Thread.currentThread().getName());
+            }
+        };
+        System.out.println("当前时间: " + new Date() + "n" +
+                "线程名称: " + Thread.currentThread().getName());
+        Timer timer = new Timer("Timer");
+        long delay = 1000L;
+        timer.schedule(task, delay);
+
+
+
+    }
 }
