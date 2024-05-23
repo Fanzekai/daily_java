64c29f93d2f3388e982671beff98eb93ea8a9c68
fzk <1610661203@qq.com>
2024-05-23 20:10:35 +0800

    commit demo14

---
+++ src/main/java/demo14/DesignPatterns/CreateDemo/BuilderPattern/Builder1.java
@@ -0,0 +1,6 @@
+package demo14.DesignPatterns.CreateDemo.BuilderPattern;/**
+ * @author  fzk
+ * @date  2024/5/23  19:56
+ * @version 1.0
+*/public class Builder1 {
+}
---
+++ src/main/java/demo14/DesignPatterns/CreateDemo/BuilderPattern/Product.java
@@ -0,0 +1,6 @@
+package demo14.DesignPatterns.CreateDemo.BuilderPattern;/**
+ * @author  fzk
+ * @date  2024/5/23  19:55
+ * @version 1.0
+*/public class Product {
+}
