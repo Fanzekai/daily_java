49054aee360af57c28534895047657ab5b3b2579
“fzekai” <fzekai@linewell.com>
2024-12-16 17:32:19 +0800

    新增注释

--- src/main/java/demo01/AopDemo/dmeo01/LoggingAspect.java
+++ src/main/java/demo01/AopDemo/dmeo01/LoggingAspect.java
@@ -15,18 +15,42 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class LoggingAspect {
 
+    /**
+     * 在执行UserService的getUserById方法之前执行的通知。
+     *
+     * @return 无返回值
+     */
     // Before advice
     @Before("execution(* UserService.getUserById(..))")
     public void logBefore() {
         System.out.println("Before executing getUserById");
     }
 
+    /**
+     * 这是一个切面（Aspect）中的通知（Advice），它使用了Spring AOP（面向切面编程）的功能。
+     *
+     * @After注解表明这是一个后置通知，它会在目标方法正常完成后执行。
+     * @param "execution(* UserService.getUserById(..))" 指定了切点表达式，
+     * 表示该通知将应用于UserService类中getUserById方法的执行。
+     * 此处的*表示匹配任何返回类型，(..)表示匹配任何参数列表。
+     *
+     * 当UserService类中的getUserById方法正常执行完毕后，
+     * 此方法（logAfter）将被调用，输出一条日志信息，表明getUserById方法已经执行完毕。
+     */
     // After advice
     @After("execution(* UserService.getUserById(..))")
     public void logAfter() {
         System.out.println("After executing getUserById");
     }
 
+    /**
+     * 环绕通知（Around Advice）
+     * 该通知方法会在目标方法执行前后进行拦截，允许在目标方法执行前后添加自定义逻辑。
+     *
+     * @param joinPoint 连接点对象，提供了对拦截方法的反射访问
+     * @return 目标方法的执行结果
+     * @throws Throwable 如果在通知方法或目标方法中抛出异常，该异常将被抛出
+     */
     // Around advice
     @Around("execution(* UserService.getUserById(..))")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
